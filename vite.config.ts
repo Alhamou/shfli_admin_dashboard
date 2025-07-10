27b188f00b529c2fe1028b0c081c1d47c233ea1a
alhamou <emadalhamo@gmail.com>
2025-07-10 21:14:30 +0200

    set base

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,14 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  base: './dist/',
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
   },
-    build: {
+  build: {
     outDir: 'dist',
   },
 })
